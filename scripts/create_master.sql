fdc90bcee0d3be86e670120a2f2367801aa6f5d9
camilo <camilo@camilos-MacBook-Pro.local>
2023-10-16 13:54:15 +0200

    Adding replica identity

--- scripts/create_master.sql
+++ scripts/create_master.sql
@@ -5,6 +5,7 @@ product_name text,
   order_date date,
   CONSTRAINT pk_orders PRIMARY KEY(id)
 );
+ALTER TABLE orders REPLICA IDENTITY USING INDEX pk_orders;
 CREATE TABLE orders_partitioned (
           id serial,
 product_name text,
@@ -16,6 +17,7 @@ CREATE TABLE orders_partitioned_2023_10 PARTITION OF orders_partitioned
     FOR VALUES FROM ('2023-10-01') TO ('2023-10-31');
 CREATE TABLE orders_partitioned_2023_11 PARTITION OF orders_partitioned
     FOR VALUES FROM ('2023-11-01') TO ('2023-11-30');
+ALTER TABLE orders_partitioned REPLICA IDENTITY USING INDEX pk_orders_partitioned;
 CREATE ROLE repl_user WITH REPLICATION LOGIN PASSWORD 'repl_user';
 GRANT ALL PRIVILEGES ON DATABASE testdb TO repl_user;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO repl_user;
